7d1c6052dce9dcadb0e58046c3c3ef1d9c14ed63
yanitejeda <tejedasp@gmail.com>
2021-10-26 14:53:46 +0200

    inserisco i singoli elementi

--- script.js
+++ script.js
@@ -1,98 +1,126 @@
+const boxContainer = document.querySelector('.box-container');
+console.log(boxContainer)
 const iconArray = [
 	{
-		name: 'cat',
+		nome: 'cat',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'crow',
+		nome: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'dog',
+		nome: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'dove',
+		nome: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'dragon',
+		nome: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'horse',
+		nome: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'hippo',
+		nome: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'fish',
+		nome: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas'
 	},
 	{
-		name: 'carrot',
+		nome: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas'
 	},
 	{
-		name: 'apple-alt',
+		nome: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas'
 	},
 	{
-		name: 'lemon',
+		nome: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas'
 	},
 	{
-		name: 'pepper-hot',
+		nome: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas'
 	},
 	{
-		name: 'user-astronaut',
+		nome: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	},
 	{
-		name: 'user-graduate',
+		nome: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	},
 	{
-		name: 'user-ninja',
+		nome: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	},
 	{
-		name: 'user-secret',
+		nome: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	}
 ];
+
+
+
+
+const cardIcon = iconArray.map((iconSingleArray) => {
+
+//inserisco il mio html nel dom 
+    boxContainer.innerHTML += ` 
+    
+    <div class="card-box-container">
+    <div class="card-img-logo">
+      <i class="${iconSingleArray.family} ${iconSingleArray.prefix + iconSingleArray.nome}"></i>
+    </div>
+    <div class="card-name-text">
+      <div class="card-title text-center">${iconSingleArray.nome}</div>
+    </div>
+    </div>`
+
+
+    return {
+        indice: iconSingleArray,
+		
+    }
+})
+console.log(cardIcon)
+
